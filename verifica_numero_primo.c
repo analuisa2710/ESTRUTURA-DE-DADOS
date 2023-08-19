0cc08a16251b0d96bb7b7c81484621f896427c44
Ana Luísa Da Penha Matos <113694235+analuisa2710@users.noreply.github.com>
2023-08-19 10:27:36 -0300

    Update verifica_numero_primo.c

--- verifica_numero_primo.c
+++ verifica_numero_primo.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char* argv[]){
-    int num, i;
-
-    num = atoi(argv[1]);
-
-    if(num <= 1){
-        printf("0");
-    }else{
-        for(i=3; i < num; i+=2){
-            if(num%i==0){
-                printf("0");
-                break;
-            }
-        }
-        if(i==num){
-            printf("1");
+int ehPrimo(int numero) {
+    if (numero <= 1) {
+        return 0; 
+    }
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return 0; 
         }
     }
+    return 1; 
+}
 
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <numero>\n", argv[0]);
+        return 1;
+    }
+
+    int numero = atoi(argv[1]);
+
+    if (ehPrimo(numero)) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
 }
